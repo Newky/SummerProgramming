b553b0773b19b082b310536b05042daf885c9310
Gavin Delaney <gavodel@gmail.com>
2012-06-27 19:09:20 +0100

    functions and for loops and if first time

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,13 @@
-name = 'gavin delaney'
-print "Hello %s!!!" % (name)
+def number1(small,big):
+	for i in range(small, big):
+		print "player %d"%i
+
+def number2(small,big):
+	for i in range(small,big):
+		print "player %d"%i
+	
+j=7
+if j < 6:
+		number1(0,6)
+else:
+	number2(6,11)
